c95e88058b5c87f49c39033a6c78ba2548f43962
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-16 11:05:50 +0530

    Added GridView

--- lib/screen/catalog/catalog_screen.dart
+++ lib/screen/catalog/catalog_screen.dart
@@ -1,24 +1,45 @@
+import 'package:ecommerce_app/model/category_model.dart';
+import 'package:ecommerce_app/model/product_model.dart';
 import 'package:ecommerce_app/widgets/custom_navbar.dart';
+import 'package:ecommerce_app/widgets/product_card.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CatalogScreen extends StatelessWidget {
-  const CatalogScreen({Key? key}) : super(key: key);
-
   static const String routeName = '/catalog';
 
-  static Route route() {
+  const CatalogScreen({Key? key, required this.category}) : super(key: key);
+
+  static Route route({required Category category}) {
     return MaterialPageRoute(
       settings: const RouteSettings(name: '/error'),
-      builder: (_) => CatalogScreen(),
+      builder: (_) => CatalogScreen(category: category),
     );
   }
 
+  final Category category;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Catalog'),
+    final List<Product> categoryProducts = Product.products
+        .where((product) => product.category == category.name)
+        .toList();
+    return Scaffold(
+      appBar: CustomAppBar(title: category.name),
       bottomNavigationBar: CustomNavBar(),
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 1.15),
+        itemCount: categoryProducts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+              child: ProductCard(
+            product: categoryProducts[index],
+            widthFactor: 2.2,
+          ));
+        },
+      ),
     );
   }
 }
