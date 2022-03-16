2afc78b6453e7321621ebf41e6019f25cb3988b8
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-16 12:18:30 +0530

    Changed code to fix product in HeroCarousel

--- lib/widgets/hero_carousel_card.dart
+++ lib/widgets/hero_carousel_card.dart
@@ -1,18 +1,28 @@
-import 'package:ecommerce_app/model/category_model.dart';
 import 'package:flutter/material.dart';
 
+import 'package:ecommerce_app/model/category_model.dart';
+import 'package:ecommerce_app/model/product_model.dart';
+
 class HeroCarouselCard extends StatelessWidget {
-  final Category category;
+  final Product? product;
+  final Category? category;
   const HeroCarouselCard({
     Key? key,
-    required this.category,
+    this.product,
+    this.category,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(context, '/catalog', arguments: category);
+        if (this.product == null) {
+          Navigator.pushNamed(
+            context,
+            '/catalog',
+            arguments: category,
+          );
+        }
       },
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 20),
@@ -20,8 +30,10 @@ class HeroCarouselCard extends StatelessWidget {
             borderRadius: const BorderRadius.all(Radius.circular(5.0)),
             child: Stack(
               children: <Widget>[
-                Image.network(category.imageUrl,
-                    fit: BoxFit.cover, width: 1000.0),
+                Image.network(
+                    product == null ? category!.imageUrl : product!.imageUrl,
+                    fit: BoxFit.cover,
+                    width: 1000.0),
                 Positioned(
                   bottom: 0.0,
                   left: 0.0,
@@ -39,7 +51,7 @@ class HeroCarouselCard extends StatelessWidget {
                     ),
                     padding: const EdgeInsets.symmetric(
                         vertical: 10.0, horizontal: 20.0),
-                    child: Text(category.name,
+                    child: Text(product == null ? category!.name : '',
                         style: Theme.of(context)
                             .textTheme
                             .headline2!
