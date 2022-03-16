6055f0d6d0037d1d617b0df5b758c4df4f894395
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-16 19:38:56 +0530

    Created wishlist screen

--- lib/screen/wishlist/wishlist_screen.dart
+++ lib/screen/wishlist/wishlist_screen.dart
@@ -1,6 +1,10 @@
+import 'package:ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
+import 'package:ecommerce_app/model/product_model.dart';
 import 'package:ecommerce_app/widgets/custom_navbar.dart';
+import 'package:ecommerce_app/widgets/product_card.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({Key? key}) : super(key: key);
@@ -16,9 +20,39 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Wishlist'),
-      bottomNavigationBar: CustomNavBar(),
-    );
+    return Scaffold(
+        appBar: const CustomAppBar(title: 'Wishlist'),
+        bottomNavigationBar: const CustomNavBar(),
+        body: BlocBuilder<WishlistBloc, WishlistState>(
+          builder: (context, state) {
+            if (state is WishlistLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is WishlistLoaded) {
+              return GridView.builder(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 16.0,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1, childAspectRatio: 2.2),
+                itemCount: state.wishlist.products.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Center(
+                      child: ProductCard(
+                    product: state.wishlist.products[index],
+                    widthFactor: 1.1,
+                    leftPosition: 100,
+                    isWishlist: true,
+                  ));
+                },
+              );
+            } else {
+              return const Text('Something went wrong');
+            }
+          },
+        ));
   }
 }
