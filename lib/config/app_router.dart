6c20d02d624d8bfdb15561fed3baf742b90c56ae
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-16 11:05:04 +0530

    Added argument in Catalog Screen

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/model/category_model.dart';
 import 'package:ecommerce_app/screen/cart/cart_screen.dart';
 import 'package:ecommerce_app/screen/screen.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,9 @@ class AppRouter {
       case WishlistScreen.routeName:
         return WishlistScreen.route();
       case CatalogScreen.routeName:
-        return CatalogScreen.route();
+        return CatalogScreen.route(
+          category: settings.arguments as Category,
+        );
       // case SplashScreen.routeName:
       //   return SplashScreen.route();
       // case CheckoutScreen.routeName:
