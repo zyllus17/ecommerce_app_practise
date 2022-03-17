a133f81b24ab65a2cbb66a33233020fb7984fa70
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-17 21:28:27 +0530

    Created Cart constructor

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -2,82 +2,12 @@ import 'package:ecommerce_app/model/model.dart';
 import 'package:equatable/equatable.dart';
 
 class Cart extends Equatable {
-  Cart();
-  // final List<Product> products;
+  final List<Product> products;
 
-  // const Cart({this.products = const <Product>[]});
-
-  // @override
-  // List<Object?> get props => [products];
+  const Cart({this.products = const <Product>[]});
 
   @override
-  List<Object?> get props => [];
-
-  static List<Product> products = const [
-    Product(
-      name: 'Soft Drink #2',
-      category: 'Soft Drinks',
-      imageUrl:
-          'https://images.unsplash.com/photo-1610873167013-2dd675d30ef4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=488&q=80', //https://unsplash.com/photos/Viy_8zHEznk
-      price: 2.99,
-      isRecommended: false,
-      isPopular: true,
-    ),
-    Product(
-      name: 'Soft Drink #3',
-      category: 'Soft Drinks',
-      imageUrl:
-          'https://images.unsplash.com/photo-1603833797131-3c0a18fcb6b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', //https://unsplash.com/photos/5LIInaqRp5s
-      price: 2.99,
-      isRecommended: true,
-      isPopular: true,
-    ),
-    Product(
-      name: 'Smoothies #1',
-      category: 'Smoothies',
-      imageUrl:
-          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
-      price: 2.99,
-      isRecommended: true,
-      isPopular: false,
-    ),
-    Product(
-      name: 'Smoothies #1',
-      category: 'Smoothies',
-      imageUrl:
-          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
-      price: 2.99,
-      isRecommended: true,
-      isPopular: false,
-    ),
-    Product(
-      name: 'Smoothies #1',
-      category: 'Smoothies',
-      imageUrl:
-          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
-      price: 2.99,
-      isRecommended: true,
-      isPopular: false,
-    ),
-    Product(
-      name: 'Smoothies #1',
-      category: 'Smoothies',
-      imageUrl:
-          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
-      price: 2.99,
-      isRecommended: true,
-      isPopular: false,
-    ),
-    Product(
-      name: 'Smoothies #1',
-      category: 'Smoothies',
-      imageUrl:
-          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
-      price: 2.99,
-      isRecommended: true,
-      isPopular: false,
-    ),
-  ];
+  List<Object?> get props => [products];
 
   // Map productQuantity(products) {
   //   var quantity = Map();
