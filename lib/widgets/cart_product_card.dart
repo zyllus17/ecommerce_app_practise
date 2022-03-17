2c894396326ad967cb025ee5ed696f40c5e951c6
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-17 21:30:15 +0530

    Added BlocBuilder Cartbloc

--- lib/widgets/cart_product_card.dart
+++ lib/widgets/cart_product_card.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_app/bloc/cart/cart_bloc.dart';
 import 'package:ecommerce_app/model/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartProductCard extends StatelessWidget {
   const CartProductCard({
@@ -41,21 +43,29 @@ class CartProductCard extends StatelessWidget {
             ),
           ),
           SizedBox(width: 10),
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.remove_circle),
-                onPressed: () {},
-              ),
-              Text(
-                '1',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              IconButton(
-                icon: Icon(Icons.add_circle),
-                onPressed: () {},
-              ),
-            ],
+          BlocBuilder<CartBloc, CartState>(
+            builder: (context, state) {
+              return Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove_circle),
+                    onPressed: () {
+                      context.read<CartBloc>().add(CartProductRemoved(product));
+                    },
+                  ),
+                  Text(
+                    '1',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add_circle),
+                    onPressed: () {
+                      context.read<CartBloc>().add(CartProductAdded(product));
+                    },
+                  ),
+                ],
+              );
+            },
           ),
         ],
       ),
