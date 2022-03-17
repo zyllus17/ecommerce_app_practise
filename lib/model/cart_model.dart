ce1da31732df5f4bad06c34f8a3492fb615d0f4f
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-17 22:04:34 +0530

    Added Map productQuantity

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -9,19 +9,17 @@ class Cart extends Equatable {
   @override
   List<Object?> get props => [products];
 
-  // Map productQuantity(products) {
-  //   var quantity = Map();
-
-  //   products.forEach((product) {
-  //     if (!quantity.containsKey(product)) {
-  //       quantity[product] = 1;
-  //     } else {
-  //       quantity[product] += 1;
-  //     }
-  //   });
-
-  //   return quantity;
-  // }
+  Map productQuantity(products) {
+    var quantity = Map();
+    products.forEach((product) {
+      if (!quantity.containsKey(product)) {
+        quantity[product] = 1;
+      } else {
+        quantity[product] += 1;
+      }
+    });
+    return quantity;
+  }
 
   double get subtotal =>
       products.fold(0, (total, current) => total + current.price);
