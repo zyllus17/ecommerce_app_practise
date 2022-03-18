9b7e7809a58e4bbf1deb921b6a73865ae4dc8d1e
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-18 22:51:10 +0530

    Added Category and Product bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,46 @@
+import 'package:ecommerce_app/bloc/cart/cart_bloc.dart';
+import 'package:ecommerce_app/bloc/category/category_bloc.dart';
+import 'package:ecommerce_app/bloc/product/product_bloc.dart';
 import 'package:ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
 import 'package:ecommerce_app/config/app_router.dart';
 import 'package:ecommerce_app/config/theme.dart';
-import 'package:ecommerce_app/screen/screen.dart';
+import 'package:ecommerce_app/repositories/category/category_repository.dart';
+import 'package:ecommerce_app/screen/splash/splash_screen.dart';
 import 'package:ecommerce_app/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'bloc/cart/cart_bloc.dart';
+import 'repositories/product/product_repository.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist())),
         BlocProvider(create: (_) => CartBloc()..add(CartStarted())),
+        BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist())),
+        BlocProvider(
+          create: (_) => CategoryBloc(
+            categoryRepository: CategoryRepository(),
+          )..add(LoadCategories()),
+        ),
+        BlocProvider(
+          create: (_) => ProductBloc(
+            productRepository: ProductRepository(),
+          )..add(LoadProducts()),
+        )
       ],
       child: MaterialApp(
-        title: 'Zero to unicorn',
+        title: 'Zero To Unicorn',
+        debugShowCheckedModeBanner: false,
         theme: theme(),
         onGenerateRoute: AppRouter.onGenerateRoute,
         initialRoute: SplashScreen.routeName,
