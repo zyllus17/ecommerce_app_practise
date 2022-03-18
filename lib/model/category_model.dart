9715a6a2a0e0a138eb35b210cf56161fe8a74f23
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-18 22:51:48 +0530

    Added Category fromSnapshot

--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -1,15 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class Category extends Equatable {
   final String name;
   final String imageUrl;
+
   const Category({
     required this.name,
     required this.imageUrl,
   });
 
   @override
-  List<Object?> get props => [name, imageUrl];
+  List<Object?> get props => [
+        name,
+        imageUrl,
+      ];
+
+  static Category fromSnapshot(DocumentSnapshot snap) {
+    Category category = Category(
+      name: snap['name'],
+      imageUrl: snap['imageUrl'],
+    );
+    return category;
+  }
 
   static List<Category> categories = const [
     Category(
