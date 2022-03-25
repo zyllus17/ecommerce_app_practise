ba3d2571f36beebaddd895e02bca29ba8094f4a0
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-25 17:09:40 +0530

    Added route name to CustomNav

--- lib/screen/catalog/catalog_screen.dart
+++ lib/screen/catalog/catalog_screen.dart
@@ -26,7 +26,7 @@ class CatalogScreen extends StatelessWidget {
         .toList();
     return Scaffold(
       appBar: CustomAppBar(title: category.name),
-      bottomNavigationBar: CustomNavBar(),
+      bottomNavigationBar: CustomNavBar(screen: routeName),
       body: GridView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
