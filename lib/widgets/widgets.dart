bc2197900f548b6913f7554f8b6b9ecbb7aade51
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-03-25 17:19:14 +0530

    Added Order_Summary

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,6 +2,7 @@ export 'cart_product_card.dart';
 export 'custom_appbar.dart';
 export 'custom_navbar.dart';
 export 'hero_carousel_card.dart';
+export 'order_summary.dart';
 export 'product_card.dart';
 export 'product_carousel.dart';
 export 'section_title.dart';
