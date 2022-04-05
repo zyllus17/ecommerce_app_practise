95958c0c877e12b2391fa26fb17e6598a1739ff2
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-04-06 00:04:33 +0530

    Updated to flutter_bloc to 8.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,36 @@
-import 'package:ecommerce_app/bloc/cart/cart_bloc.dart';
-import 'package:ecommerce_app/bloc/category/category_bloc.dart';
-import 'package:ecommerce_app/bloc/checkout/checkout_bloc.dart';
-import 'package:ecommerce_app/bloc/product/product_bloc.dart';
-import 'package:ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
+import 'package:ecommerce_app/bloc/bloc.dart';
 import 'package:ecommerce_app/config/app_router.dart';
 import 'package:ecommerce_app/config/theme.dart';
-import 'package:ecommerce_app/repositories/category/category_repository.dart';
-import 'package:ecommerce_app/repositories/checkout/checkout_repository.dart';
-import 'package:ecommerce_app/screen/splash/splash_screen.dart';
+import 'package:ecommerce_app/repositories/repositories.dart';
+import 'package:ecommerce_app/screen/screen.dart';
 import 'package:ecommerce_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'repositories/product/product_repository.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  Bloc.observer = SimpleBlocObserver();
-  runApp(MyApp());
+  BlocOverrides.runZoned(() {
+    runApp(const MyApp());
+  }, blocObserver: SimpleBlocObserver());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => CartBloc()..add(CartStarted())),
+        BlocProvider(create: (_) => CartBloc()..add(LoadCart())),
         BlocProvider(
           create: (context) => CheckoutBloc(
             cartBloc: context.read<CartBloc>(),
             checkoutRepository: CheckoutRepository(),
           ),
         ),
-        BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist())),
+        BlocProvider(create: (_) => WishlistBloc()..add(LoadWishlist())),
         BlocProvider(
           create: (_) => CategoryBloc(
             categoryRepository: CategoryRepository(),
