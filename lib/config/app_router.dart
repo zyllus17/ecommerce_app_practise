c9df27cdc4fefe40909c61f8dd9d0fcaf1fe7dbd
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-04-06 00:05:06 +0530

    Removed cart_screen import

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/model/category_model.dart';
 import 'package:ecommerce_app/model/product_model.dart';
-import 'package:ecommerce_app/screen/cart/cart_screen.dart';
 import 'package:ecommerce_app/screen/screen.dart';
 import 'package:flutter/material.dart';
 
