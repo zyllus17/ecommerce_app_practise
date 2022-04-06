e1c73b122259e04460bf9d15dfbc5ff79059c05f
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-04-06 14:36:55 +0530

    Created OrderConfirmation screen

---
+++ lib/screen/order_confirmation/order_confirmation_screen.dart
@@ -0,0 +1,103 @@
+import 'package:ecommerce_app/model/model.dart';
+import 'package:ecommerce_app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OrderConfirmation extends StatelessWidget {
+  static const String routeName = '/order-confirmation';
+  const OrderConfirmation({Key? key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (context) => OrderConfirmation(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Order Confirmation'),
+      bottomNavigationBar: const CustomNavBar(screen: routeName),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: double.infinity,
+                  height: 300,
+                ),
+                Positioned(
+                  left: (MediaQuery.of(context).size.width - 100) / 2,
+                  top: 125,
+                  child: SvgPicture.asset('assets/svgs/garlands.svg'),
+                ),
+                Positioned(
+                  top: 250,
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'Your order is complete!',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3!
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hi Massimo,',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Thank you for purchasing on Zero To Unicorn.',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'ORDER CODE: #k321-ekd3',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  OrderSummary(),
+                  SizedBox(height: 20),
+                  Text(
+                    'ORDER DETAILS',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Divider(thickness: 2),
+                  SizedBox(height: 5),
+                  ListView(
+                    shrinkWrap: true,
+                    padding: EdgeInsets.zero,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      OrderSummaryProductCard(
+                        product: Product.products[0],
+                        quantity: 2,
+                      ),
+                      OrderSummaryProductCard(
+                        product: Product.products[1],
+                        quantity: 3,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
